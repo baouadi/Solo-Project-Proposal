25f5f057b2f141274e1e07f5fdf72d2715e887ef
baouadi <bassem.aouadi9@gmail.com>
2024-05-03 19:16:53 +0100

    setting up the project

---
+++ Pizza Time/PizzaTime/src/main/java/com/project/pizzatime/PizzaTimeApplication.java
@@ -0,0 +1,13 @@
+package com.project.pizzatime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzaTimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzaTimeApplication.class, args);
+	}
+
+}
